6ed358e1758346ecd64b0a2cb9c0f8e3d9db3a55
Ryan Mehr <ryanmehr2005.development@gmail.com>
2025-10-09 22:14:27 -0400

    Unit tests / isolated testing is done for at least one critical module (supplies).

--- app/tests/supplies/test.py
+++ app/tests/supplies/test.py
@@ -6,7 +6,28 @@ from ...tests.reusableFixtures import reusableFixtures
 
 rf = reusableFixtures.rf
 uf = reusableFixtures.uf
-mock_supply = reusableFixtures.mock_supply
+
+@pytest.fixture
+def mock_supply():
+    supply_given = {
+        'id':1,
+        'name':"Fertilizer",
+        'type':"Nutrient Supply",
+        'quantity':3,
+        'unit':"Field 1",
+        }
+    return supply_given
+
+@pytest.fixture
+def new_mock_supply():
+    supply_info_change = {
+        'id':1,
+        'name':"Pesticide",
+        'type':"Bugs Be Gone",
+        'quantity': 5,
+        'unit': 'Field 2',
+    }
+    return supply_info_change
 
 @pytest.mark.django_db
 def test_supplies_list(rf, uf, mocker):
@@ -61,7 +82,7 @@ def test_add_supplies(rf, uf, mock_supply, mocker):
     assert b'Field 1' in response_2.content
 
 @pytest.mark.django_db
-def test_edit_supplies(rf, uf, mock_supply, mocker):
+def test_edit_supplies(rf, uf, mock_supply, new_mock_supply, mocker):
     assert Supplies.objects.count() == 0
 
     mock_fetch = mocker.patch(
@@ -77,15 +98,7 @@ def test_edit_supplies(rf, uf, mock_supply, mocker):
     mock_fetch.assert_called_once()
     assert Supplies.objects.count() == 1
 
-    supply_info_change = {
-        'id':1,
-        'name':"Pesticide",
-        'type':"Bugs Be Gone",
-        'quantity': 5,
-        'unit': 'Field 2',
-    }
-
-    request_2 = rf.post('/supplies/edit/', data=supply_info_change)
+    request_2 = rf.post('/supplies/edit/', data=new_mock_supply)
     request_2.user = uf
     response_2 = supplies.edit_supplies(request_2)
 
@@ -105,5 +118,32 @@ def test_edit_supplies(rf, uf, mock_supply, mocker):
 
 
 @pytest.mark.django_db
-def test_delete_supplies(rf, uf, mocker):
-    return
+def test_delete_supplies(rf, uf, mock_supply, mocker):
+    assert Supplies.objects.count() == 0
+
+    mock_fetch = mocker.patch(
+        'app.logging.logging.Logger.log'
+    )
+
+    request = rf.post('/supplies/add/', data=mock_supply)
+    request.user = uf
+    response = supplies.add_supplies(request)
+
+    assert response.status_code == 302
+    mock_fetch.assert_called_once()
+    assert Supplies.objects.count() == 1
+
+    request_2 = rf.post('/supplies/delete/', data=mock_supply)
+    request_2.user = uf
+    response_2 = supplies.delete_supplies(request_2)
+
+    assert response_2.status_code == 302
+    assert Supplies.objects.count() == 0
+
+    request_3 = rf.get('/supplies/')
+    request_3.user = uf
+    response_3 = supplies.supplies_list(request_3)
+    
+    assert response_3.status_code == 200
+    assert Supplies.objects.count() == 0
+    assert b'No supply records found' in response_3.content
