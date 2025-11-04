869ebbf34a8f7b7b7f1925b1d9acb3d1fd280095
Ryan Mehr <ryanmehr2005.development@gmail.com>
2025-11-04 09:11:11 -0500

    Proper OOP implementation fixed memory and speed issues!
    Thank you!!!!!!

--- app/logging/logging.py
+++ app/logging/logging.py
@@ -4,54 +4,18 @@ import gc
 
 #from memory_profiler import profile
 
-# Making it so that it can do both files, whichever gets passed in.
-def rotate_if_needed(filename, max_bytes, backup_count):
-    if (
-            os.path.exists(filename)
-            and os.path.getsize(filename) >= max_bytes
-        ):
-            for i in range(backup_count, 0, -1):
-                backup_file = f"{filename}.{i}"
-                prev_file = f"{filename}.{i-1}" if i > 1 else filename
-                if os.path.exists(prev_file):
-                    os.rename(prev_file, backup_file)
+class activityObject:
+    def __init__(self, activity, location, user, time):
+        self.activity = activity
+        self.location = location
+        self.user = user
+        self.time = time
 
-def formatLine(line_given):
-    """
-    Formats line for retrieve_recent_activities function in logging.py
-
-    LINE FORMAT TO GIVE
-    -------------------
-    Format : YEAR-MONTH-DAY HOUR:MINUTE:SECOND User {user name} {action} {location}: {object name}
-    Example : "2025-11-02 16:09:38 User ryan deleted supply: Test Supply C"
-    """
-    class activityObject:
-            def __init__(self, activity, location, user, time):
-                self.activity = activity
-                self.location = location
-                self.user = user
-                self.time = time
-
-            def __str__(self):
-                return f"Activity: {self.activity}, Location: {self.location}, User: {self.user}, Time: {self.time}"
-
-    chars_to_remove = ["[","]"]
-
-    for char in chars_to_remove:
-        line_given = line_given.replace(char, "")
-
-    line_given = line_given.split()
-
-    the_activity = " ".join(line_given[2:])
-    the_location = (line_given[5].capitalize())[:-1]
-    the_user = line_given[3]
-    the_time = " ".join(line_given[0:2])
-
-    line_object = activityObject(the_activity, the_location, the_user, the_time)
-
-    return line_object
-
+    def __str__(self):
+        return f"Activity: {self.activity}, Location: {self.location}, User: {self.user}, Time: {self.time}"
+            
 class Logger:
+        
     def __init__(self, filename="app.log", modifications_filename="modification_activities.log", max_bytes=1024 * 1024, first_backup_count=3, second_backup_count=3):
         self.filename = filename
         # modification_activites.log file MUST be in the same directory as app.log. Unless we should put modification_activites.log in the instantiation of Logger of all other files.
@@ -60,14 +24,53 @@ class Logger:
         self.first_backup_count = first_backup_count
         self.second_backup_count = second_backup_count
 
+    # Making it so that it can do both files, whichever gets passed in.
+    def __rotate_if_needed(filename, max_bytes, backup_count):
+        if (
+                os.path.exists(filename)
+                and os.path.getsize(filename) >= max_bytes
+            ):
+                for i in range(backup_count, 0, -1):
+                    backup_file = f"{filename}.{i}"
+                    prev_file = f"{filename}.{i-1}" if i > 1 else filename
+                    if os.path.exists(prev_file):
+                        os.rename(prev_file, backup_file)
+    
+    # Formatting lines for activities.
+    def __formatLine(line_given):
+        """
+        Formats line for retrieve_recent_activities function in logging.py
+
+        LINE FORMAT TO GIVE
+        -------------------
+        Format : YEAR-MONTH-DAY HOUR:MINUTE:SECOND User {user name} {action} {location}: {object name}
+        Example : "2025-11-02 16:09:38 User ryan deleted supply: Test Supply C"
+        """
+
+        chars_to_remove = ["[","]"]
+
+        for char in chars_to_remove:
+            line_given = line_given.replace(char, "")
+
+        line_given = line_given.split()
+
+        the_activity = " ".join(line_given[2:])
+        the_location = (line_given[5].capitalize())[:-1]
+        the_user = line_given[3]
+        the_time = " ".join(line_given[0:2])
+
+        line_object = activityObject(the_activity, the_location, the_user, the_time)
+
+        return line_object
+
     def log(self, message):
-        rotate_if_needed(self.filename, self.max_bytes, self.first_backup_count)
+        Logger.__rotate_if_needed(self.filename, self.max_bytes, self.first_backup_count)
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(self.filename, "a") as f:
-            f.write(f"[{timestamp}] {message}\n")
+           f.write(f"[{timestamp}] {message}\n")
 
         if ("added" in message) or ("edited" in message) or ("deleted" in message):
-            rotate_if_needed(self.modifications_filename, self.max_bytes, self.second_backup_count)
+            Logger.__rotate_if_needed(self.modifications_filename, self.max_bytes, self.second_backup_count)
             with open(self.modifications_filename, "a") as f:
                 f.write(f"[{timestamp}] {message}\n")
 
@@ -101,7 +104,7 @@ class Logger:
                 lines = f.readlines()
 
                 for line in reversed(lines):
-                    recent_activity_array.append(formatLine(line))
+                    recent_activity_array.append(Logger.__formatLine(line))
                     if (not get_all and len(recent_activity_array) >= amount_to_retrieve):
                         break
 
