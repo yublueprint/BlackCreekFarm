492c5373f44d5644e7c0ce208aa488c49501ed16
Amna Sohail <amna.sohail7866@gmail.com>
2025-11-12 17:17:06 -0500

    Fixed error

--- app/tests/livestock/test_edit_livestock.py
+++ app/tests/livestock/test_edit_livestock.py
@@ -8,7 +8,6 @@ pytestmark = pytest.mark.django_db
 def test_edit_livestock_success(client, user, livestock, mocker):
     mock_logger = mocker.patch("app.backend.livestock.livestock.logger.log")
 
-    original_name = livestock.name
 
     response = client.post(
         reverse("edit_livestock"),
@@ -19,8 +18,7 @@ def test_edit_livestock_success(client, user, livestock, mocker):
     assert livestock.name == "Updated Name"
     assert response.status_code == 302
     assert response.url == reverse("livestock_list")
-    mock_logger.assert_any_call(f"User {user} edited livestock: {original_name} to {livestock.name}")
-
+    mock_logger.assert_any_call(f"User {user} edited livestock: {livestock.name}")
 
 def test_edit_livestock_redirect_on_get(client, user):
     response = client.get(reverse("edit_livestock"))
